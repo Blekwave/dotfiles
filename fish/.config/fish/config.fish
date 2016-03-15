1121d04c0aca91e864252be90cfdd6bbcb587419
Victor Diniz <blek.net@gmail.com>
2016-03-14 21:38:23 -0300

    fish: ssh-agent support, new git aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,13 @@
 # Use with caution. Made for GNU Stow.
 
 set -g -x fish_greeting ''
+
+# Load code on startup
+set includes_dir $HOME/.config/fish/include
+for file in $includes_dir/*.fish
+    . $file
+end
+
+# Initialize ssh-agent
+start_agent
+
---
+++ fish/.config/fish/include/aliases.fish
@@ -0,0 +1,5 @@
+alias gst 'git status'
+alias gc 'git commit'
+alias gp 'git push'
+alias gl 'git pull'
+alias ga 'git add'
