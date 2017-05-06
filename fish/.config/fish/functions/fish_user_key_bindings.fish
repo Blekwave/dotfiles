b3f872403f2a661a04efd8bd9ba35f44fe55ad76
Victor Diniz <blek.net@gmail.com>
2017-05-06 13:15:23 -0300

    vim: ignore .netrwhist, some fish key binding changes

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,5 @@
-command --search fzf_key_bindings >/dev/null; and begin
-    fzf_key_bindings
+function fish_user_key_bindings
+	if type fzf ^/dev/null >/dev/null
+        fzf_key_bindings
+end
 end
--- fish/.config/fish/include/gems.fish
+++ fish/.config/fish/include/gems.fish
@@ -1,3 +1,3 @@
-command --search gem >/dev/null; and begin
-    set -gx PATH (gem environment gempath | sed "s/:/\n/g" | head -n 1)/bin $PATH
-end;
+# command --search gem >/dev/null; and begin
+#     set -gx PATH (gem environment gempath | sed "s/:/\n/g" | head -n 1)/bin $PATH
+# end;
