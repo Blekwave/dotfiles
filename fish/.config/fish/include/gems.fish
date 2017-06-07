524726a94c975355090904dcc552ddb85efdacd6
Victor Diniz <blek.net@gmail.com>
2017-06-07 08:11:34 -0300

    fish: delete old workaround fish scripts

--- fish/.config/fish/include/gems.fish
+++
@@ -1,3 +0,0 @@
-# command --search gem >/dev/null; and begin
-#     set -gx PATH (gem environment gempath | sed "s/:/\n/g" | head -n 1)/bin $PATH
-# end;
--- fish/.config/fish/include/perlbrew.fish
+++
@@ -1,4 +0,0 @@
-set pb_fish_script ~/perl5/perlbrew/etc/perlbrew.fish
-# if test -e $pb_fish_script
-#     . $pb_fish_script
-# end
